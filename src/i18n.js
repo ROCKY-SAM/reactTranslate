c29869d8d82afee3bb32e032c9efdb2b75305f79
sameera <abeysekaramadushanka14@gmail.com>
2021-09-20 01:37:15 +0530

    fix locale issue

--- src/i18n.js
+++ src/i18n.js
@@ -3,9 +3,22 @@ import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next} from 'react-i18next';
 
+
+import translationEN from "./locales/en/translation.json";
+import translationSi from "./locales/si/translation.json";
+const resources = {
+ en: {
+   translation: translationEN,
+ },
+ si: {
+   translation: translationSi,
+ },
+};
 i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
     fallbackLng:'en',
     debug:true,
+    lng: "en",
+    resources,
     detection:{
         order:['queryString','cookie'],
         cache:['cookie']
