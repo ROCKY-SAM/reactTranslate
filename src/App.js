009a7c0473036943fc80082e8c9e1e1b317f4497
sameera <abeysekaramadushanka14@gmail.com>
2021-09-20 01:10:19 +0530

    ui changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Form } from 'semantic-ui-react'
 import './App.css';
 import {Trans, useTranslation} from 'react-i18next';
 import { Button,Divider } from 'semantic-ui-react';
-import { Flag } from 'semantic-ui-react'
+import { Flag ,Header} from 'semantic-ui-react'
 function App() {
   
   const {t,i18n} = useTranslation()
@@ -21,7 +21,7 @@ function App() {
   return (
     <div className="App">
 
-<h3><Trans i18nKey="description"></Trans></h3>
+<Header size='medium'><Trans i18nKey="description"></Trans></Header>
 
     <Button basic color='blue' onClick={()=> changeLanguage("en")}><Flag name='gb eng' />English</Button>
     <Button basic color='violet' onClick={()=> changeLanguage("si")}><Flag name='lk' />සිංහල</Button>
